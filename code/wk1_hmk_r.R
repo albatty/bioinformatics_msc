3ecaddd412c0e4c67d0d6e273ded24957d67f4f9
albatty <alexander.battison@gmail.com>
2021-10-12 11:01:48 +0100

    deleted some work to test the branching function

--- code/wk1_hmk_r.R
+++ code/wk1_hmk_r.R
@@ -16,25 +16,3 @@ se <- function(x){
   std_er <- sd(x)/sqrt(length(x))
   return(std_er)
 }
-##set i to standard error of vector b
-i <- se(x = b)
-##create vector naming mean, sd, and se
-j = c("mean" = g, "sd" = h, "se" = i)
-j
-##create sequence from 15 to 100
-seq = c(15:100)
-##calculate mean of numbers between 20 and 60
-(mean(new_seq <- seq[seq>20 & seq<60]))
-##sum the numbers un seq that are greater than 48
-sum(newnew_seq <- seq[seq>48])
-##  function that returns the min and max of seq
-find_min_max <- function(u){
-  ##get minimum from first value of sequence
-  min <- seq[1]
-  seq_rev <- sort(seq, decreasing = TRUE)
-  ##get maximum from first value of reversed sequence
-  max <- seq_rev[1]
-  ans <- c(min, max)
-  return(ans)
-}
-find_min_max(u = seq)
